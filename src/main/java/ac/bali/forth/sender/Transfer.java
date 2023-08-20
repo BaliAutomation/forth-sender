eed8a011f8265fec72bbb16e73d6bac0095a4d96
niclas <niclas@hedhman.org>
2023-08-20 11:14:15 +0200

    Moving colors to the Forth runtime.

--- src/main/java/ac/bali/forth/sender/Transfer.java
+++ src/main/java/ac/bali/forth/sender/Transfer.java
@@ -9,15 +9,14 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class Transfer
-{
+public class Transfer {
     public static final String ANSI_RESET = "\u001B[0m";
-//    public static final String ANSI_BLACK = "\u001B[30m";
+    //    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-//    public static final String ANSI_YELLOW = "\u001B[33m";
+    //    public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
-//    public static final String ANSI_MAGENTA = "\u001B[35m";
+    //    public static final String ANSI_MAGENTA = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
 //    public static final String ANSI_WHITE = "\u001B[37m";
 //    public static final String ANSI_DARK_GREY = "\u001B[90m";
@@ -29,7 +28,7 @@ public class Transfer
 //    public static final String ANSI_BRIGHT_CYAN = "\u001B[96m";
 //    public static final String ANSI_BRIGHT_WHITE = "\u001B[97m";
 
-    public static final byte[] CR = new byte[] { 13 };
+    public static final byte[] CR = new byte[]{13};
     public static final String COMPILER_ERROR = "Compiler Error";
     private static SerialPort commPort;
 
@@ -38,29 +37,23 @@ public class Transfer
     private int here1;
 
     public static void main(String[] args)
-        throws Exception {
+            throws Exception {
 
         instance = new Transfer();
         parseCmdLine(args, file -> {
-            try
-            {
+            try {
                 instance.transfer(file);
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println();
                 System.err.println();
                 commPort.closePort();
-                if( ! e.getMessage().equals(COMPILER_ERROR)) {
+                if (!e.getMessage().equals(COMPILER_ERROR)) {
                     throw new UndeclaredThrowableException(e);
                 }
-            }
-            finally
-            {
-                try
-                {
+            } finally {
+                try {
                     Thread.sleep(100);
-                } catch (Exception e)
-                {
+                } catch (Exception e) {
                     // ignore
                 }
             }
@@ -68,8 +61,7 @@ public class Transfer
     }
 
     public Transfer()
-        throws Exception
-    {
+            throws Exception {
 
         String serialPort = "/dev/ttyACM0";
         commPort = SerialPort.getCommPort(serialPort);
@@ -86,26 +78,20 @@ public class Transfer
 
     public static void parseCmdLine(String[] args, Consumer<String> then) throws IOException {
         String file = args[0];
-        if (file.startsWith("@"))
-        {
+        if (file.startsWith("@")) {
             List<String> files = Files.readAllLines(Paths.get(file.substring(1)));
-            for (String filename : files)
-            {
+            for (String filename : files) {
                 filename = filename.trim();
-                if( !filename.startsWith("#"))
-                {
+                if (!filename.startsWith("#")) {
                     then.accept(filename);
                 }
             }
-        }
-        else
-        {
+        } else {
             then.accept(file);
         }
     }
 
-    public void transfer(String file)
-    {
+    public void transfer(String file) {
         try {
             System.out.println(ANSI_GREEN + file + ANSI_RESET);
             System.out.println();
@@ -120,21 +106,18 @@ public class Transfer
 
                     int matchesUpTo = match(line, echo);
                     if (matchesUpTo != line.length() || line.length() != echo.length() - 14) {
-                        if (!echo.endsWith("ok.")) {
-                            System.out.println();
-                            System.out.println();
-                            System.out.println("    Sent:  " + line);
-                            System.out.println("Received:  " + echo);
-                            throw new RuntimeException(COMPILER_ERROR);
-                        }
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("    Sent:  " + line);
+                        System.out.println("Received:  " + echo);
+                        throw new RuntimeException(COMPILER_ERROR);
                     }
                 }
             }
             int here = here();
             System.out.println("Size:" + (here - here1) + "  (Total: " + (here - here0) + ")    (Free: " + (0x20010000 - here) + ")");
             here1 = here;
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -146,22 +129,19 @@ public class Transfer
         return Integer.parseInt(parts[3], 16);
     }
 
-    private void sendCr()
-    {
+    private void sendCr() {
         commPort.writeBytes(CR, 1);
     }
 
     private void drain()
-        throws InterruptedException
-    {
+            throws InterruptedException {
         System.out.println("Draining...");
         sendCr();
         sendCr();
         sendCr();
         Thread.sleep(100);
         byte[] buffer = new byte[2048];
-        while (commPort.bytesAvailable() > 0)
-        {
+        while (commPort.bytesAvailable() > 0) {
             commPort.readBytes(buffer, 1);
             System.out.print(new String(buffer, 0, 1));
             //noinspection BusyWait
@@ -173,53 +153,41 @@ public class Transfer
     private void writeLine(SerialPort commPort, String line) {
         byte[] bytes = line.getBytes();
         int bytesWritten = commPort.writeBytes(bytes, bytes.length);
-        if (bytesWritten != bytes.length)
-        {
+        if (bytesWritten != bytes.length) {
             System.err.println(ANSI_RED + "Couldn't write all of:" + line + ANSI_RESET);
             throw new RuntimeException("Write timeout");
         }
         sendCr();
     }
 
-    private String readLine(SerialPort commPort)
-    {
+    private String readLine(SerialPort commPort) {
         byte[] buffer = new byte[1];
         StringBuilder result = new StringBuilder();
-        while (true)
-        {
-            if (commPort.readBytes(buffer, 1) > 0)
-            {
+        while (true) {
+            if (commPort.readBytes(buffer, 1) > 0) {
                 char c = (char) buffer[0];
-                if (c == 13 || c == 10)
-                {
+                if (c == 13 || c == 10) {
                     return result.toString();
-                }
-                else
-                {
+                } else {
                     result.append(c);
                 }
 
-            }
-            else
-            {
+            } else {
                 throw new RuntimeException("Read Timeout");
             }
         }
     }
 
-    private int match(String sent, String received)
-    {
-        if( received.startsWith(sent) && received.endsWith(ANSI_CYAN + "ok." + ANSI_RESET))
+    private int match(String sent, String received) {
+        if (received.startsWith(sent) && (received.endsWith(ANSI_CYAN + "ok." + ANSI_RESET) || received.endsWith(ANSI_BLUE + "ok'" + ANSI_RESET)))
             return sent.length();
         byte[] buf1 = sent.getBytes();
         byte[] buf2 = received.getBytes();
         int length = received.length();
-        for (int i = 0; i < length - 1; i++)
-        {
-            if( i == buf1.length)
+        for (int i = 0; i < length - 1; i++) {
+            if (i == buf1.length)
                 return i;
-            if (buf1[i] != buf2[i])
-            {
+            if (buf1[i] != buf2[i]) {
                 return i;
             }
         }
